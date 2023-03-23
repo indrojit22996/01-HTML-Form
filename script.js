5ff00b7d78c08951a383482e34c628993d5edec9
indrojit22996 <indrojit22996bhaduri@gmail.com>
2023-03-23 20:46:44 +0530

    HTML Form

---
+++ script.js
@@ -0,0 +1,10 @@
+let nameI = document.querySelector(".name").value;
+let nameE = document.querySelector(".email").value;
+let nameP = document.querySelector(".num").value;
+let btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+  console.log(nameI);
+  console.log(nameE);
+  console.log(nameP);
+});
